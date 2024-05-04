23e6d416b28841821844ac75dbfd717ef65de9a0
Yamileth Pereyra <190244@mail.unacar.mx>
2024-05-04 14:15:37 -0600

    Se agrega el proyecto

---
+++ Music/js/music-list.js
@@ -0,0 +1,93 @@
+
+let allMusic = [
+  {
+    name: "29 (Official Live Performance) | Vevo",
+    artist: "Demi Lovato",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "The River",
+    artist: "Aurora",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Genesis",
+    artist: "Grimes",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Say a Little Prayer (Live)",
+    artist: "Lianne La Havas",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Frequency",
+    artist: "RoKakkmaddafakkay",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "You Are The Light - 432hz Healing Frequency - Channelling To Raise Your Vibration",
+    artist: "Chantress Seba",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "10. MAZAS Y CATAPULTAS Prod JAVATO JONES y GONZALO LASHERAS",
+    artist: "Kase.O",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "17",
+    artist: "Zhavia",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Milagro y Desastre",
+    artist: "Silvana Estrada",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Antes De Ti",
+    artist: "Mon Laferte",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "Back To Black",
+    artist: "Amy Winehouse",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: " I'd Rather Go Blind ",
+    artist: "Beyonce",
+    img: "music-12",
+    src: "music-12"
+  },
+  {
+    name: "Killing Me Softly With His Song",
+    artist: "Fugees",
+    img: "music-13",
+    src: "music-13"
+  },
+  {
+    name: "More than words",
+    artist: "Noora",
+    img: "music-14",
+    src: "music-14"
+  },{
+    name: "lovely",
+    artist: "Billie Eilish, Khalid",
+    img: "music-15",
+    src: "music-15"
+  },
+  
+];
